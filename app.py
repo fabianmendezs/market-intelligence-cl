d5af9e71aac640510210edc7ed5c22b3eb76acab
fabianmendezs <fabianrodrigo.mendez@gmail.com>
2026-05-19 22:54:41 -0400

    Actualiza página web v2

--- app.py
+++ app.py
@@ -198,10 +198,11 @@ fig.update_layout(
         bgcolor="rgba(0,0,0,0)",
     ),
     hovermode="x unified",
+    dragmode=False,
     margin=dict(l=0, r=0, t=50, b=0),
 )
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True, config={"scrollZoom": False, "displayModeBar": False})
 
 st.divider()
 
